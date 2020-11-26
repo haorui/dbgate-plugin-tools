a04a4412d529532feb9ef706c4ebffbe5eca32fc
Jan Prochazka <jan.prochazka@gmail.com>
2020-11-26 13:06:47 +0100

    use packed plugin

--- bin/dbgate-plugin.js
+++ bin/dbgate-plugin.js
@@ -3,12 +3,23 @@
 const rimraf = require('rimraf');
 const path = require('path');
 const os = require('os');
+const fs = require('fs');
 const ncp = require('ncp');
+const pacote = require('pacote');
 
-const packageName = process.argv[2]
-if (!packageName) throw new Error('Missing package name')
+const packageName = process.argv[2];
+if (!packageName) throw new Error('Missing package name');
 
-const pluginDir = path.join(os.homedir(), 'dbgate-data', 'plugins', packageName)
+const pluginDir = path.join(os.homedir(), 'dbgate-data', 'plugins', packageName);
 
 rimraf.sync(pluginDir)
-ncp(process.cwd(), pluginDir)
+
+const tgzFile = fs.readdirSync(process.cwd()).find(x => x.endsWith('.tgz'));
+
+if (tgzFile) {
+  pacote.extract(tgzFile, pluginDir).then(() => {
+    fs.unlink(tgzFile, () => {});
+  });
+} else {
+  ncp(process.cwd(), pluginDir);
+}
